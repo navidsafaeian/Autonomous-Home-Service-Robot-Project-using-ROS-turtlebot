72db15fea135ed9be929bf90a41904fbe846efb6
nsafa <neodirects@gmail.com>
2018-10-17 20:07:49 -0600

    second commit for src

--- src/ShellScripts/add_markers.sh
+++ src/ShellScripts/add_markers.sh
@@ -3,6 +3,6 @@ xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/wo
 sleep 5
 xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/src/World/myMap.yaml" &
 sleep 5
-xterm -e "roslaunch turtlebot_rviz_launchers  view_navigation.launch" &
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
 xterm -e "rosrun add_markers add_markers"
--- src/ShellScripts/test_slam.sh
+++ src/ShellScripts/test_slam.sh
@@ -5,5 +5,5 @@ xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers  view_navigation.launch" &
 sleep 5
-xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch" &
+xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch" 
 
